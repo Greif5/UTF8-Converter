b21e2cd84c85368cb56a231745cc5ba81d39c5f4
Greif5 <sebastianxxl@msn.com>
2018-02-20 18:38:50 +0100

    Added a catch

--- src/Converter.java
+++ src/Converter.java
@@ -3,22 +3,26 @@ import java.io.*;
 public class Converter {
 
 	public static void main(String[] args) throws IOException { 
-		File fFileIn = new FileChooser().getFile();
-		String strNewName = fFileIn.getParent() + "/UTF8_" +fFileIn.getName();
+		try{
+			File fFileIn = new FileChooser().getFile();
 		
-		String charsetOrg = "ISO-8859-1";
-		String charsetNew = "UTF-8";
-		String strLine = "";
-		
-		BufferedReader brIn		= new BufferedReader( new InputStreamReader (new FileInputStream(fFileIn), charsetOrg));
-		PrintWriter pwPrinter	= new PrintWriter(strNewName,charsetNew);
-		
-		while( (strLine = brIn.readLine()) != null) { 
-			pwPrinter.println(strLine);
+			String strNewName = fFileIn.getParent() + "/UTF8_" +fFileIn.getName();
+			
+			String charsetOrg = "ISO-8859-1";
+			String charsetNew = "UTF-8";
+			String strLine = "";
+			
+			BufferedReader brIn		= new BufferedReader( new InputStreamReader (new FileInputStream(fFileIn), charsetOrg));
+			PrintWriter pwPrinter	= new PrintWriter(strNewName,charsetNew);
+			
+			while( (strLine = brIn.readLine()) != null) { 
+				pwPrinter.println(strLine);
+			}
+			
+			brIn.close();
+			pwPrinter.close();
 		}
-		
-		brIn.close();
-		pwPrinter.close();
+		catch(Exception e){	e.printStackTrace();	}
 	}
 
 }
